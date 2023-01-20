adaaba40b89a64cde91521510d958bc6d57bd850
Kapil Sinha <ksinha1999@gmail.com>
2023-01-20 02:36:40 -0800

    Add $ values in the swap UI, Phat Contract driver script, autoformatting

--- src/components/Modal/TokenModal.tsx
+++ src/components/Modal/TokenModal.tsx
@@ -1,8 +1,5 @@
 import {
   Box,
-  Button,
-  Flex,
-  Link,
   Modal,
   ModalOverlay,
   ModalContent,
@@ -10,14 +7,14 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-  Text, Input,
+  Text,
+  Input,
 } from "@chakra-ui/react";
 // Can use later to make the table pretty but requires more work
 // import ReactTable from "react-table";
 import { useEffect, useState, Fragment } from "react";
-import { Token } from '../../data_models/Token';
-import token_list from './token_list.json';
-
+import { Token } from "../../data_models/Token";
+import token_list from "./token_list.json";
 
 type Props = {
   isOpen: any;
@@ -28,8 +25,15 @@ type Props = {
   setSelectedToken: any;
 };
 
-export default function TokenModal({isOpen, onClose, selectedChain, selectedToken, otherToken, setSelectedToken}: Props) {
-  const crypto = token_list.map(x => Token.fromJSON(x));
+export default function TokenModal({
+  isOpen,
+  onClose,
+  selectedChain,
+  selectedToken,
+  otherToken,
+  setSelectedToken,
+}: Props) {
+  const crypto = token_list.map((x) => Token.fromJSON(x));
   // console.log(crypto);
   const [search, setSearch] = useState<any>("");
 
@@ -47,7 +51,8 @@ export default function TokenModal({isOpen, onClose, selectedChain, selectedToke
         border="0.06rem"
         borderStyle="solid"
         borderColor="gray.300"
-        borderRadius="3xl">
+        borderRadius="3xl"
+      >
         <ModalHeader color="black" px={4} fontSize="lg" fontWeight="medium">
           Select A Token
         </ModalHeader>
@@ -56,7 +61,8 @@ export default function TokenModal({isOpen, onClose, selectedChain, selectedToke
           fontSize="sm"
           _hover={{
             color: "gray.600",
-          }}/>
+          }}
+        />
         <ModalBody pt={0} px={4}>
           <Box
             borderRadius="3xl"
@@ -66,7 +72,8 @@ export default function TokenModal({isOpen, onClose, selectedChain, selectedToke
             px={5}
             pt={4}
             pb={2}
-            mb={3}>
+            mb={3}
+          >
             <Input
               placeholder="Search token name / symbol / address"
               fontSize="1.5rem"
@@ -80,14 +87,17 @@ export default function TokenModal({isOpen, onClose, selectedChain, selectedToke
               onChange={(e) => {
                 setSearch(e.target.value);
               }}
-              color="black"/>
+              color="black"
+            />
           </Box>
           {/*<Box color="black" display="flex">*/}
           {/*    <img src={imageToShow} alt="logo" width="50px" />*/}
           {/*    <span>{tokenNameToShow}</span>*/}
           {/*</Box>*/}
           <div id="tokenlist" className="App">
-            <table style={{'height': '350px', 'overflow':'scroll', 'display': 'block'}}>
+            <table
+              style={{ height: "350px", overflow: "scroll", display: "block" }}
+            >
               <thead>
                 <tr>
                   <td>Name</td>
@@ -97,52 +107,72 @@ export default function TokenModal({isOpen, onClose, selectedChain, selectedToke
               </thead>
               {/* <tbody> */}
               <tbody>
-                {crypto
-                  .filter((token) => {
-                    return token.symbol.toLowerCase().includes(search.toLowerCase()) ||
-                    token.name.toLowerCase().includes(search.toLowerCase()) ||
-                    token.getAddressFromEncodedTokenName().includes(search.toLowerCase());
-                  })
-                  .map((token, index) => {
-                    let hidden = false;
-                    // Hide a token if it is selected on the other dropdown
-                    // TODO: revisit, should only kick in if they're the same chain
-                    let sameAsOtherToken = token.name === otherToken?.name && token.chain === otherToken?.chain;
-                    let wrongChain = token.chain !== selectedChain;
-                    if (sameAsOtherToken || wrongChain) {
-                      hidden = true
-                    }
-                    // I altogether avoid creating the document elements so we can highlight alternating rows
-                    // and also it's wasteful to render but hide the objects
-                    return hidden === false && (
-                      <Fragment key={index}>
-                        <tr id={token.name} key={index}
-                            style={{
-                              backgroundColor: (token.name === selectedToken?.name ? "rgb(208, 172, 235)" : "")
-                            }}
-                            hidden={hidden}
-                            onClick={function (e) {
-                              // console.log('Set token');
-                              setSelectedToken(token);
-                            }}>
-                          <td className="logo">
-                              {/* <a href={token.websiteUrl}>
+                {
+                  crypto
+                    .filter((token) => {
+                      return (
+                        token.symbol
+                          .toLowerCase()
+                          .includes(search.toLowerCase()) ||
+                        token.name
+                          .toLowerCase()
+                          .includes(search.toLowerCase()) ||
+                        token
+                          .getAddressFromEncodedTokenName()
+                          .includes(search.toLowerCase())
+                      );
+                    })
+                    .map((token, index) => {
+                      let hidden = false;
+                      let sameAsOtherToken =
+                        token.getAddressFromEncodedTokenName() ===
+                          otherToken?.getAddressFromEncodedTokenName() &&
+                        token.chain === otherToken?.chain;
+                      let wrongChain = token.chain !== selectedChain;
+                      if (sameAsOtherToken || wrongChain) {
+                        hidden = true;
+                      }
+                      // I altogether avoid creating the document elements so we can highlight alternating rows
+                      // and also it's wasteful to render but hide the objects
+                      return (
+                        hidden === false && (
+                          <Fragment key={index}>
+                            <tr
+                              id={token.name}
+                              key={index}
+                              style={{
+                                backgroundColor:
+                                  token.getAddressFromEncodedTokenName() ===
+                                  selectedToken?.getAddressFromEncodedTokenName()
+                                    ? "rgb(208, 172, 235)"
+                                    : "",
+                              }}
+                              hidden={hidden}
+                              onClick={function (e) {
+                                // console.log('Set token');
+                                setSelectedToken(token);
+                              }}
+                            >
+                              <td className="logo">
+                                {/* <a href={token.websiteUrl}>
                                 <img src={token.icon} alt="logo" width="30px" />
                               </a> */}
-                              <span>{token.name}</span>
-                          </td>
-                          <td className="symbol">{token.symbol}</td>
-                          <td className="address">{token.getTruncatedAddressFromEncodedTokenName()}</td>
-                        </tr>
-                      </Fragment>
-                    );
-                  })
+                                <span>{token.name}</span>
+                              </td>
+                              <td className="symbol">{token.symbol}</td>
+                              <td className="address">
+                                {token.getTruncatedAddressFromEncodedTokenName()}
+                              </td>
+                            </tr>
+                          </Fragment>
+                        )
+                      );
+                    })
                   //.slice(0, 10)
                 }
               </tbody>
             </table>
           </div>
-
         </ModalBody>
 
         <ModalFooter
@@ -150,11 +180,9 @@ export default function TokenModal({isOpen, onClose, selectedChain, selectedToke
           background="rgb(237, 238, 242)"
           borderBottomLeftRadius="3xl"
           borderBottomRightRadius="3xl"
-          p={6}>
-          <Text
-            color="black"
-            fontWeight="medium"
-            fontSize="md">
+          p={6}
+        >
+          <Text color="black" fontWeight="medium" fontSize="md">
             Manage Token List
           </Text>
         </ModalFooter>
--- src/phat_api/privadex_phat_contract_api.js
+++ src/phat_api/privadex_phat_contract_api.js
@@ -1,133 +1,173 @@
-import { ApiPromise, WsProvider, Keyring } from '@polkadot/api';
-import { typeDefinitions } from '@polkadot/types';
-import { ContractPromise } from '@polkadot/api-contract';
-import { create as phala_create, signCertificate as phala_signCertificate, types as phala_types } from '@phala/sdk';
+import { ApiPromise, WsProvider, Keyring } from "@polkadot/api";
+import { typeDefinitions } from "@polkadot/types";
+import { ContractPromise } from "@polkadot/api-contract";
+import {
+  create as phala_create,
+  signCertificate as phala_signCertificate,
+  types as phala_types,
+} from "@phala/sdk";
 
-import privadex_executor_contract from './privadex_executor_contract.json';
-
+import privadex_executor_contract from "./privadex_executor_contract.json";
 
 function loadPrivadexContractFile(address) {
-    const metadata = privadex_executor_contract;
-    // console.log('Metadata:', metadata);
-    // const constructor = metadata.V3.spec.constructors.find(c => c.label == 'default').selector;
-    const constructor = metadata.V3.spec.constructors.find(c => c.label === 'new').selector;
-    const name = metadata.contract.name;
-    const wasm = metadata.source.wasm;
-    return { wasm, metadata, constructor, name, address };
+  const metadata = privadex_executor_contract;
+  // console.log('Metadata:', metadata);
+  // const constructor = metadata.V3.spec.constructors.find(c => c.label == 'default').selector;
+  const constructor = metadata.V3.spec.constructors.find(
+    (c) => c.label === "new"
+  ).selector;
+  const name = metadata.contract.name;
+  const wasm = metadata.source.wasm;
+  return { wasm, metadata, constructor, name, address };
 }
 
 async function contractApi(api, pruntimeUrl, contract) {
-    const newApi = await api.clone().isReady;
-    const phala = await phala_create({ api: newApi, baseURL: pruntimeUrl, contractId: contract.address, autoDeposit: true });
-    const contractApi = new ContractPromise(
-        phala.api,
-        contract.metadata,
-        contract.address,
-    );
-    contractApi.sidevmQuery = phala.sidevmQuery;
-    contractApi.instantiate = phala.instantiate;
-    return contractApi;
+  const newApi = await api.clone().isReady;
+  const phala = await phala_create({
+    api: newApi,
+    baseURL: pruntimeUrl,
+    contractId: contract.address,
+    autoDeposit: true,
+  });
+  const contractApi = new ContractPromise(
+    phala.api,
+    contract.metadata,
+    contract.address
+  );
+  contractApi.sidevmQuery = phala.sidevmQuery;
+  contractApi.instantiate = phala.instantiate;
+  return contractApi;
 }
 
 export class PrivaDexAPI {
-    #contractApi;
-    #certSudo;
+  #contractApi;
+  #certSudo;
 
-    constructor(contractApi, certSudo) {
-        this.#contractApi = contractApi;
-        this.#certSudo = certSudo;
+  constructor(contractApi, certSudo) {
+    this.#contractApi = contractApi;
+    this.#certSudo = certSudo;
+  }
+
+  static async initialize() {
+    const nodeUrl = "wss://poc5.phala.network/ws";
+    const pruntimeUrl = "https://poc5.phala.network/tee-api-1";
+    const sudoAccount = "//Alice";
+
+    const contractPrivadex = loadPrivadexContractFile(
+      "0xcd62e9590b807b11bfe8ba97937864f99974d23976d127b89550805ea3fa1df0"
+    );
+
+    // Connect to the chain
+    const wsProvider = new WsProvider(nodeUrl);
+    const api = await ApiPromise.create({
+      provider: wsProvider,
+      types: {
+        ...phala_types,
+        GistQuote: {
+          username: "String",
+          accountId: "AccountId",
+        },
+        ...typeDefinitions.contracts.types,
+      },
+    });
+
+    // Prepare accounts
+    const keyring = new Keyring({ type: "sr25519" });
+    const sudo = keyring.addFromUri(sudoAccount);
+    // console.log(`Sudo has address ${sudo.address} with publicKey [${sudo.publicKey}]`);
+
+    const certSudo = await phala_signCertificate({ api, pair: sudo });
+    // console.log("certSudo (Alice):", certSudo)
+
+    const privadexContractApi = await contractApi(
+      api,
+      pruntimeUrl,
+      contractPrivadex
+    );
+    // console.log("PrivaDEX API:", privadexContractApi, privadexContractApi.query);
+
+    // privadexApi.query functions:
+    // computeExecutionPlan
+    // executionPlanStepForward
+    // getAdmin
+    // getEscrowEthAccountAddress
+    // getExecPlan
+    // getExecplanIds
+    // initSecretKeys
+    // quote
+    // startSwap
+    return new PrivaDexAPI(privadexContractApi, certSudo);
+  }
+
+  async escrowEthAddress() {
+    const escrowAddress =
+      await this.#contractApi.query.getEscrowEthAccountAddress(
+        this.#certSudo,
+        {}
+      );
+    console.log(
+      "PrivaDEX escrow =",
+      escrowAddress,
+      escrowAddress.output.asOk.toString()
+    );
+    return escrowAddress.output.asOk.toString();
+  }
+
+  async quote(
+    srcChain,
+    destChain,
+    srcTokenEncoded,
+    destTokenEncoded,
+    amountIn
+  ) {
+    // Backend returns (quote in destToken, amountIn in $ x 1e6, quote in $ x 1e6)
+    // This function returns (quote in destToken, amountIn in $, quote in $)
+    const quoteInfo = await this.#contractApi.query.quote(
+      this.#certSudo,
+      {},
+      srcChain,
+      destChain,
+      srcTokenEncoded,
+      destTokenEncoded,
+      amountIn.toString()
+    );
+    // console.log("PrivaDEX quote =", amountIn.toString(), quoteInfo);
+    if (quoteInfo.output.isOk) {
+      let tup = quoteInfo.output.asOk;
+      // console.log(tup[0].toBigInt(), tup[1] / 1e6, tup[2] / 1e6);
+      return [tup[0].toBigInt(), tup[1] / 1e6, tup[2] / 1e6];
     }
+    return [0n, 0, 0];
+  }
 
-    static async initialize() {
-        const nodeUrl = 'wss://poc5.phala.network/ws';
-        const pruntimeUrl = 'https://poc5.phala.network/tee-api-1';
-        const sudoAccount = '//Alice';
-
-        const contractPrivadex = loadPrivadexContractFile('0xcd9cb06dd6c9d8613f8ceb906b72903e365991ce6a0149bfc0c54816c8d68569');
-
-        // Connect to the chain
-        const wsProvider = new WsProvider(nodeUrl);
-        const api = await ApiPromise.create({
-            provider: wsProvider,
-            types: {
-                ...phala_types,
-                'GistQuote': {
-                    username: 'String',
-                    accountId: 'AccountId',
-                },
-                ...typeDefinitions.contracts.types,
-            }
-        });
-
-        // Prepare accounts
-        const keyring = new Keyring({ type: 'sr25519' });
-        const sudo = keyring.addFromUri(sudoAccount);
-        // console.log(`Sudo has address ${sudo.address} with publicKey [${sudo.publicKey}]`);
-
-        const certSudo = await phala_signCertificate({ api, pair: sudo });
-        // console.log("certSudo (Alice):", certSudo)
-
-        const privadexContractApi = await contractApi(api, pruntimeUrl, contractPrivadex);
-        // console.log("PrivaDEX API:", privadexContractApi, privadexContractApi.query);
-
-        // privadexApi.query functions:
-        // computeExecutionPlan
-        // executionPlanStepForward
-        // getAdmin
-        // getEscrowEthAccountAddress
-        // getExecPlan
-        // getExecplanIds
-        // initSecretKeys
-        // quote
-        // startSwap
-        return new PrivaDexAPI(privadexContractApi, certSudo);
-    }
-
-    async escrowEthAddress() {
-        const escrowAddress = await this.#contractApi.query.getEscrowEthAccountAddress(this.#certSudo, {},);
-        console.log("PrivaDEX escrow =", escrowAddress, escrowAddress.output.asOk.toString());
-        return escrowAddress.output.asOk.toString();
-    }
-
-    async quote(srcChain, destChain, srcTokenEncoded, destTokenEncoded, amountIn) {
-        const quote = await this.#contractApi.query.quote(
-            this.#certSudo,
-            {},
-            srcChain,
-            destChain,
-            srcTokenEncoded,
-            destTokenEncoded,
-            amountIn.toString()
-        );
-        // console.log("PrivaDEX quote =", amountIn.toString(), quote);
-        // console.log(quote.output.asOk.toBigInt());
-        return quote.output.asOk.toBigInt(); 
-    }
-
-    async startSwap(
-        userToEscrowTransferEthTxn,
-        srcChain,
-        destChain,
-        srcEthAddress,
-        destEthAddress,
-        srcTokenEncoded,
-        destTokenEncoded,
-        amountIn
-    ) {
-        const exec_plan_uuid = await this.#contractApi.query.startSwap(
-            this.#certSudo,
-            {},
-            userToEscrowTransferEthTxn.replace("0x", ""),
-            srcChain,
-            destChain,
-            srcEthAddress.replace("0x", ""),
-            destEthAddress.replace("0x", ""),
-            srcTokenEncoded,
-            destTokenEncoded,
-            amountIn.toString()
-        )
-        let uuid = '0x' + exec_plan_uuid.output.asOk.map(x => x.toString(16).padStart(2, '0')).join('');
-        console.log("PrivaDEX UUID =", uuid);
-        return uuid;
-    }
+  async startSwap(
+    userToEscrowTransferEthTxn,
+    srcChain,
+    destChain,
+    srcEthAddress,
+    destEthAddress,
+    srcTokenEncoded,
+    destTokenEncoded,
+    amountIn
+  ) {
+    const exec_plan_uuid = await this.#contractApi.query.startSwap(
+      this.#certSudo,
+      {},
+      userToEscrowTransferEthTxn.replace("0x", ""),
+      srcChain,
+      destChain,
+      srcEthAddress.replace("0x", ""),
+      destEthAddress.replace("0x", ""),
+      srcTokenEncoded,
+      destTokenEncoded,
+      amountIn.toString()
+    );
+    let uuid =
+      "0x" +
+      exec_plan_uuid.output.asOk
+        .map((x) => x.toString(16).padStart(2, "0"))
+        .join("");
+    console.log("PrivaDEX UUID =", uuid);
+    return uuid;
+  }
 }
